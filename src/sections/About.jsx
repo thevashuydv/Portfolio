e722623cae225c23ca4c427a46434fab292bf18b
“thevashuydv” <vashuyadav2804@gmail.com>
2025-08-02 21:59:59 +0530

    last correction

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -19,8 +19,8 @@ const About = () => {
           <div className="z-10">
             <p className="headtext">Hi, I'm Vashu Yadav</p>
             <p className="subtext">
-              Over the last 4 years, I developed my frontend and backend dev
-              skills to deliver dynamic and software and web applications.
+              Over the last 2 years, I developed my frontend and backend dev
+              skills to deliver  software and web applications.
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
